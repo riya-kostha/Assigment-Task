d523490be4830ebed7feed5d9d02f3ea4d465713
riya73514-8363 <70106834+riya73514-8363@users.noreply.github.com>
2020-08-25 12:26:38 +0530

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+#include"Header.h"
+int main()
+{
+
+	std::cout << "hii frist program of cpp";
+	int c = sum();
+	std::cout <<"\n";
+	std::cout << "  sum is=";
+	std::cout << c;
+	return 0;
+}
