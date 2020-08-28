eba31dda5051a57cb70ecca88190dd763eca7d22
riya73514-8363 <70106834+riya73514-8363@users.noreply.github.com>
2020-08-28 23:28:05 +0530

    Add files via upload

--- CPractice/Source.cpp
+++ CPractice/Source.cpp
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
-
+#define a 10
 //#define prod(a,b) a*b 
 int main()
 {
-    int a = 3,b=6;
+ //   int a = 3,b=6;
     //int x = 0, y = 10;
    /* int x = 11,y=0;
      
@@ -20,7 +20,7 @@ int main()
     }
   
     printf("  x= %d y= %d", x, y);
-    */
+    
 
     
 
@@ -29,6 +29,61 @@ int main()
     //i = 2;
     printf("%c", *str+1);
     printf("%c", *(str+1));
+    */
+
+
+   // enum MyEnum{one=1,two=2,three=3};
+    
+  //  enum MyEnum m=1;
+
+   // int c = m;
+    //printf("%d", m);
+    /*char s[5];
+    if (printf("helo")==3)
+    {
+        printf("true");
+    }
+    else {
+
+        printf("false");
+    }
+    */
+   //int i = 1;
+    // 2 + ++i;
+    //printf("%d", i);
+   // int i;
+    //for (i++;i < 10;i = 0)
+    //{
+
+      //  printf("%d",i);
+    //}
+
+    //printf("%d", printf("%d", printf("hello")));
+
+    //printf("%d",sizeof(a*2));
+ 
+    int i = 1,x = 1;
+        while (i <5)
+        {
+            switch (i)
+            {
+            case (1):
+            case 2:
+            case 3:
+                printf("hello\n");
+                break;
+                break;
+            case 4:
+                printf("hi");
+                break;
+               
+            default:
+                break;
+            }
+          
+            i++;
+    }
+        
 
     return 0;
 }
