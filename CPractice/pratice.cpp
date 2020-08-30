6ee9642b6e4776a6abaaf8ec2dad62637b175e1e
riya73514-8363 <70106834+riya73514-8363@users.noreply.github.com>
2020-08-30 22:54:32 +0530

    Add files via upload

---
+++ CPractice/pratice.cpp
@@ -0,0 +1,188 @@
+#include<stdio.h>
+
+//#define max(a, b) (a>b?a:b)
+
+//#define square(x) x*x
+//#define a 1
+//#define x 1
+#define sacnf "%s Geeks"
+
+int m(static int a, static int b);
+int main()
+{
+  //  int a = 1, b = 1;
+ 
+    //printf("%d",a++||b++);
+        
+
+/*    typedef static int* i;
+    int j;
+    i a = &j;
+    printf("%d", *a);
+ 
+
+    char arr[] = "geeksforgeeks";
+    printf("%s", ? );
+   
+    int x, y = 5, z = 5;
+    x = y == z;
+    printf("%d", x);
+
+    printf(" \"GEEKS %% FOR %% GEEKS\"");
+
+    */
+   // int a[] = { 1, 2, 3, 4, 5, 6 };
+    //int* ptr = (int*)(&a + 1);
+    
+
+   // int a = 10;
+   // int* p = (int*)&a;
+
+    //printf("%d", p);
+
+  //  printf("%d\n %d",a+1,*ptr );
+
+//    printf("%s", fun() + printf("I see you"));
+  /*  int i = 5;
+    for (i; i; i)
+    {
+        printf("heloo\n");
+        i--;
+    }
+    
+
+    int i, j;
+    int p = 1, q = 1;
+
+    for (i = 0, j = 0; i < p, j < q; i++, j++)
+    {
+        printf("GeeksforGeeks\n");
+    }
+    */
+
+    //int arr[] = { 10, 20, 30, 40, 50, 60 };
+    //int* ptr1 = arr;
+    //int* ptr2 = arr + 5;
+  //  printf("ptr2 - ptr1 = %d\n", ptr2 - ptr1);
+   // printf("(char*)ptr2 - (char*) ptr1 = %d", (char*)ptr2 - (char*)ptr1);
+
+
+    
+    //printf("%d", printf("%d"));
+  //  int (*p)[10][20];
+  //  int* p[10];
+  // printf("%d", sizeof(*p));
+
+    /*int i;
+   
+   if (i = 0, 1, 2)
+        printf("Geeksforgeeks ");
+    else
+        printf("Hello ");
+        
+    printf("%d\n", i);
+    */
+
+   /* int x;
+  //  x = 36 / square(6);
+    x = square(6);
+    printf("%d", x);
+    */
+ //   int x = 2;
+  //  int k = sizeof(x == 2);
+   // int k = x == 1?2:3;
+    //printf("%d", k);
+   // signed int y= 2;
+ /*   enum A
+    {
+        //   a = 3,
+          // b , c = 5, d = 17,e
+        a, b,c, e
+    };
+    //enum A x;
+    printf("%d", e)
+    */
+   // enum day { sunday , tuesday, wednesday, thursday, friday, saturday };
+    //enum day d = thursday;
+    //int k = thursday;
+    //printf("The day number stored in d is %d %d", sunday,wednesday);
+   // char *s= "sunday";
+  /*  if (printf("%d", sunday))
+
+    {
+
+        printf("true");
+    }
+    */
+
+  // int  a = wednesday;
+   /* switch (a,a*2,a*3)
+    {
+    case 2:
+        printf("hello\n");
+    case 4:
+        printf("world\n");
+        
+    case 6:
+        printf("hhh\n");
+    default:
+        printf("no\n");
+
+    }
+    
+    //  const  int a = 50;
+
+int i = 0;
+while (i = 1)
+printf("True\n");
+printf("False\n");
+*/
+      //      int   n, b,d;
+        //    n = 1, b = 3,d=2;
+          //  int c = (n) ? d : b;
+           // printf(" value = %d", c);
+
+//char c[] = "ICRBCSIT17";
+//char* p = c;
+//printf("%c", 'A'+'0');
+//printf()
+//int x = 2;
+
+//if (a < 2)
+//{
+  //  printf("true");
+////}
+//else {
+  //  printf("false");
+//}
+/*int ch = 1,i=1;
+while (i <= 2)
+{
+    switch (++ch)
+    {
+    case a:
+        printf("hello");
+    case a + 1:
+        printf("hii");
+        //continue;
+    default:
+        break;
+        //++ch;
+    }
+}
+//++a;
+
+
+int i = 10;
+static int j = i;
+printf("%d", j);
+*/
+
+
+
+printf(sacnf,scanf);
+return 0;
+
+}
+
+
