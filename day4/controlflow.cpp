2fd5d98924dfa605822ba8176c24a920ec45e456
Riya Kostha <70106834+riya73514-8363@users.noreply.github.com>
2020-09-03 13:06:06 +0530

    Add files via upload

---
+++ day4/controlflow.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+int main()
+{
+
+
+	//control flow;
+	/*int i = 1;
+	{
+		std::cout << "hello\n";
+	}
+
+	//for (i) // this line will give an error this not right syntax
+		//std::cout << "hyy";
+		
+	int x = 2;
+	if (x)
+		std::cout << "yes";
+	std::cout << "inside if";
+	else
+		std::cout << "inside else";		//this will give error bcs we are using two statement after is without {} and there is else also;
+		*/
+
+	if (sizeof(int) > -1)
+	{
+		std::cout << "yes";
+	}
+	else {
+		std::cout << "no";
+	}
+
+
+	return 0;
+}
